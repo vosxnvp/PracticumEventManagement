1b3a9ec730cd0bab1073d91ee10e94e6d5fe0925
v.nikolaychik <vosx.nvp@gmail.com>
2026-08-09 14:42:47 +0300

    Реализованы CRUD-эндпоинты для мероприятий

---
+++ PracticumEventManagement/Controllers/EventsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticumEventManagement.Models;
+using PracticumEventManagement.Services;
+
+namespace PracticumEventManagement.Controllers;
+
+[ApiController]
+[Route("events")]
+public class EventsController : ControllerBase
+{
+    private readonly IEventService _eventService;
+
+    public EventsController(IEventService eventService)
+    {
+        _eventService = eventService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Event>> GetAll()
+    {
+        var events = _eventService.GetAll();
+
+        return Ok(events);
+    }
+
+    [HttpGet("{id:guid}")]
+    public ActionResult<Event> GetById(Guid id)
+    {
+        var eventItem = _eventService.GetById(id);
+
+        if (eventItem is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(eventItem);
+    }
+
+    [HttpPost]
+    public ActionResult<Event> Create(Event eventItem)
+    {
+        var createdEvent = _eventService.Create(eventItem);
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = createdEvent.Id },
+            createdEvent);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult Update(Guid id, Event eventItem)
+    {
+        var updated = _eventService.Update(id, eventItem);
+
+        if (!updated)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult Delete(Guid id)
+    {
+        var deleted = _eventService.Delete(id);
+
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+}
