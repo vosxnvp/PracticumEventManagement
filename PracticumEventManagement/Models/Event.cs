d8966007f947cfc0b3988991281d5d8997146402
v.nikolaychik <vosx.nvp@gmail.com>
2026-08-09 14:13:55 +0300

    Дополнена модель Event

--- PracticumEventManagement/Models/Event.cs
+++ PracticumEventManagement/Models/Event.cs
@@ -2,5 +2,14 @@
 {
     public class Event
     {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public DateTime StartAt { get; set; }
+
+        public DateTime EndAt { get; set; }
     }
 }
