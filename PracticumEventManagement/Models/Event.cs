c5b033d7313301a6e5c89dac81c2327203a8a17e
v.nikolaychik <vosx.nvp@gmail.com>
2026-08-09 14:46:52 +0300

    Добавлена базовая валидация мероприятий

--- PracticumEventManagement/Models/Event.cs
+++ PracticumEventManagement/Models/Event.cs
@@ -1,15 +1,31 @@
-﻿namespace PracticumEventManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticumEventManagement.Models
 {
-    public class Event
+    public class Event : IValidatableObject
     {
         public Guid Id { get; set; }
 
+        [Required]
         public string Title { get; set; } = string.Empty;
 
         public string? Description { get; set; }
 
+        [Required]
         public DateTime StartAt { get; set; }
 
+        [Required]
         public DateTime EndAt { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(
+            ValidationContext validationContext)
+        {
+            if (EndAt <= StartAt)
+            {
+                yield return new ValidationResult(
+                    "EndAt должен быть позже StartAt.",
+                    new[] { nameof(EndAt) });
+            }
+        }
     }
 }
